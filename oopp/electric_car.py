a768d02642be2fdac0214bcdce05ef55d9922c26
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-03 00:17:59 +0900

    added Battery class and used an instance from Battery class as attributes to the ElectricCar class

--- oopp/electric_car.py
+++ oopp/electric_car.py
@@ -44,6 +44,38 @@ class Car():
         else:
             print(f'Invalid quantity.')
 
+'''
+If the number of attributes and methods for an object in a class gets larger and too many in 
+numbers, then we can move those attributes and methods to a different class, like the bellow 
+class Battery. Initially we defined the attributes inside the ElectricCar class, but it seems
+that the number was increasing too fast. So we moved those in this class. And now we can get
+an instance of Battery class as attributes in the ElectricCar class and access all the attributes
+and methos in the similar way as done previously.
+'''
+class Battery():
+    ''' a class represents the battery attributes and methods of Electric car '''
+
+    def __init__(self, battery_size = 70):
+        ''' initialize attributes '''
+        self.battery_size = battery_size
+
+
+    def describe_battery(self):
+        ''' a method to describe the size of the battery '''
+        print(f'The car has a battery of {self.battery_size} KWh.')
+
+
+    def get_range(self):
+        ''' a method that describes the range based on the current battery size '''
+        if self.battery_size > 80:
+            range = 240
+        elif self.battery_size > 60:
+            range = 180
+        else:
+            range = 100
+        
+        print(f'The car can go around {range} on a full charge.')
+
 
 class ElectricCar(Car):
     ''' Represents an electric car '''
@@ -51,12 +83,7 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         ''' initialize all attributes '''
         super().__init__(make, model, year)
-        self.battery_size = 70
-
-    
-    def describe_battery(self):
-        ''' a method to describe the size of the battery '''
-        print(f'The car has a battery of {self.battery_size} KWh.')
+        self.battery = Battery()
 
     
     def fill_gass_tank(self, gass_to_fill):
@@ -67,5 +94,6 @@ class ElectricCar(Car):
 if __name__ == '__main__':
     my_electric_car = ElectricCar('audi', 'a4', 2020)
     print(my_electric_car.get_descriptive_name())
-    my_electric_car.describe_battery()
+    my_electric_car.battery.describe_battery()
+    my_electric_car.battery.get_range()
     my_electric_car.fill_gass_tank(10)
