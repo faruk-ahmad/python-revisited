ae200a6dd1f7528f8f236610f9deaf3a21098e73
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-02 19:38:30 +0900

    added update method for odometer reading

--- oopp/car.py
+++ oopp/car.py
@@ -20,9 +20,21 @@ class Car():
         print(f'The car has {self.odometer_reading} mileage on it.')
 
 
+    def update_odometer(self, mileage):
+        ''' a method to update the odometer_reading attribute value '''
+        self.odometer_reading = mileage
+
 
 if __name__ == '__main__':
     my_car = Car('audi', 'a4', 2014)
 
     print(my_car.get_descriptive_name())
+    my_car.read_odometer()
+
+    #modifying the attribute value directly by accesing using the instance
+    my_car.odometer_reading = 50
+    my_car.read_odometer()
+
+    #modify the attribute value using a method
+    my_car.update_odometer(100)
     my_car.read_odometer()
