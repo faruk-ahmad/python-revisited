0b62b598924c53124749ffba8928d24d1da2ecff
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-03 01:14:06 +0900

    updated reading_file.py

--- files_and_exceptions/reading_file.py
+++ files_and_exceptions/reading_file.py
@@ -5,9 +5,16 @@ Reading from file & writing to file
 file_path = './sample_input.txt'
 
 # reading the whole content of file at a time
+print('Reading whole content at a time:')
 with open(file_path, 'r') as rf:
     data = rf.read()
 
 print(data)
+
+# reading line by line
+print('\n\nReading line by line:')
+with open(file_path, 'r') as rf:
+    for line in rf:
+        print(line) 
 
 
