ab95ceec448e0d2ae060ab5b422385e60e22753a
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-17 16:20:11 +0900

    added few others important parameters in advance argparse

--- py_arguments/advanced_argparse.py
+++ py_arguments/advanced_argparse.py
@@ -15,18 +15,39 @@ my_parser = argparse.ArgumentParser(prog="Advance Argument Parser",
                                     usage="%(prog)s [options] path",
                                     description="An advance argument parser example",
                                     epilog="Enjoy the program. :)")
+"""
+default: if the argument is not given by user, the default value will be used.
+"""
+my_parser.add_argument("-p",
+                        "--path",
+                        help="path to list content.",
+                        default="/home/faruk/Desktop/exp/"
+                        )
 
-my_parser.add_argument("Path",
-                        metavar="path",
-                        type=str,
-                        help="path to list content.")
+"""
+choices: are the values from where the user can pick a value , list required
+required: if set True, force the user to input this optional argument.
+"""
+my_parser.add_argument("-show",
+                        "--show",
+                        help="Argument to show the list of directory or not",
+                        choices=["yes", "no"],
+                        default="yes",
+                        required=True
+                        )
 
 args = my_parser.parse_args()
 
-input_path = args.Path
+print(args.path)
+
+input_path = args.path
+show = args.show
+
+print(input_path)
 
 if not os.path.isdir(input_path):
     print(f'Path specified is not a directory')
     sys.exit(0)
 
-print('\n'.join(os.listdir(input_path)))
+if show == "yes":
+    print('\n'.join(os.listdir(input_path)))
